a4ef7c01fc1f11165243960d18792b8a3e7e7362
donny2131 <55747762+donny2131@users.noreply.github.com>
2020-01-08 19:28:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,4 +48,5 @@ message.channel.bulkDelete(2);msgg.delete();message.channel.send('**☑ Broadcas
 })}).catch(myst =>{msgg.edit('Timed out.');})})
 }if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
 }).catch(mys =>{msg.edit('Timed out to chose.')})})}});
+if (member.id === "256646248931655681") {
 client.login(process.env.BOT_TOKEN);
