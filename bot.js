97047aec8594d77b82202dea1be4fbbf8d384158
donny2131 <55747762+donny2131@users.noreply.github.com>
2020-02-13 23:08:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,44 +7,44 @@ let command = message.content.split(" ")[0];
 command = command.slice(prefix.length);if (command == "bc") {if(!message.member.roles.find('name','bc')) {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`ياخي لازم معك رول ادمنستريشن ولا فارق ما تقدر ترسل.`)}
 let args = message.content.split(" ").slice(1).join(" ");
-if(!args) return message.channel.send(`*اكتب الرسالة طيب ؟؟*`)
+if(!args) return message.channel.send(`اكتب الرسالة طيب ؟`)
 let filter = m => m.author.id == message.author.id
 let broadcastt = new Discord.RichEmbed().setColor('#36393e')
 .addField(`**[1] شسمة تبي ترسل للأعضاء كلهم ؟ حط رقم واحد\n\n[2] ولا تبي ترسل للأون لاين بس ؟ حط رقم ثنين\n\n[3] ولا تبي ترسل لرول معين حط رقم ثلاث عاد.\n\n[4] لو تبي ترسل كاست مع صوره حط رقم اربعة\n\n[0] ولا كنسل **`,`** **`)
-.setDescription(`**Please type the number of your chose**`)
+.setDescription(`اي شسمه اكتب اسم الرول`)
 .setFooter('you can add to the message [user] = mention the user')
 message.channel.send(broadcastt).then(msg => {
 message.channel.awaitMessages(filter, {max: 1,time: 5000,errors: ['time']})
-.then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`*خلاص انرسل الكاست.*`).then(m => {
+.then(collected => {if(collected.first().content === '1') {msg.delete(),message.channel.send(`خلاص انرسل الكاست.`).then(m => {
 message.guild.members.map(member => {setTimeout(() => {member.send(args.replace('[user]',member).replace('[icon]',`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)).then(() => {}).catch((err) => {});},);});})}
-if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`*خلاص انرسل الكاست.*`);
+if(collected.first().content === '2') {msg.delete(),message.channel.bulkDelete(1),message.channel.send(`خلاص انرسل الكاست.`);
 message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {m.send(args.replace('[user]', m))})
 message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {m.send(args.replace('[user]', m)) })
 return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {m.send(args.replace('[user]', m)) })}
-if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`*شسمه الكاست تكنسل مدري ليه.*`);}
+if(collected.first().content === '0') {msg.delete(),message.channel.bulkDelete(1);return message.channel.send(`شسمه الكاست تكنسل مدري ليه.`);}
 if(collected.first().content === '3') {msg.delete();message.channel.bulkDelete(1);
 message.channel.send('**Please Type the role name or id.**');
 message.channel.awaitMessages(filter, {max: 1,time: 40000,errors: ['time']}).then(t => {
 let R = t.first().content;
 let role = message.guild.roles.find('name',R) || message.guild.roles.get(R);
-if(!role) return message.channel.send('*ياخي مب موجود الرول ذا مدري وينه.*'),msg.delete();
+if(!role) return message.channel.send('ياخي مب موجود الرول ذا مدري وينه.'),msg.delete();
 message.channel.bulkDelete(2);
-if(role.members.size < 1) return message.channel.send('*ما اضن في احد معه الرول ذا*');;
-let XYZ = new Discord.RichEmbed().setTitle('*خلاص انرسل الكاست.*').setDescription(`*حط اسم الرول هنا*: ${role}**`).setColor(role.color)
+if(role.members.size < 1) return message.channel.send('ما اضن في احد معه الرول ذا');;
+let XYZ = new Discord.RichEmbed().setTitle('خلاص انرسل الكاست.').setDescription(`حط اسم الرول هنا: ${role}**`).setColor(role.color)
 message.channel.send(XYZ)
 message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {setTimeout(() => {n.send(args.replace('[user]',n)).catch((err) => {});});});}).catch(err =>{});}
 if(!collected.first().content.includes(['1','2','3','4','0'])) {msg.edit('Canceled.')}
 if(collected.first().content === '4') { msg.delete();
 message.channel.send('*حط رابط الصورة خلصني.*,Type "cansel" to cansel.').then(msgg =>{
 message.channel.awaitMessages(filter, {max: 1,time: 50000,errors: ['time']}).then(XX => {
-let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('*الكاست تكنسل مدري ليه.*')}
-let embed = new Discord.RichEmbed().setImage(photo).setTitle(`**are you sure you want to send this? \`[y,n]\`**`).setColor('#36393e')
-message.channel.send(embed).catch(e =>{return message.channel.send('**The Photo link is wrong :x:**')});
+let photo = XX.first().content; if(photo == 'cansel') {message.channel.bulkDelete(2); return message.channel.send('الكاست تكنسل مدري ليه.')}
+let embed = new Discord.RichEmbed().setImage(photo).setTitle(`متأكد انك تبي ترسل ذا ؟  \`[ي,لا]\`**`).setColor('#36393e')
+message.channel.send(embed).catch(e =>{return message.channel.send('رابط الصورة غلط.')});
 let filter = m => m.author.id == message.author.id
 message.channel.awaitMessages(filter, {max: 1,time: 90000,errors: ['time']}).then(XD => {if(XD.first().content === 'y') {
 let bc = new Discord.RichEmbed().setTitle(`${args}`).setImage(photo).setFooter(message.guild.name,`https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.png?size=1024`)
-message.channel.bulkDelete(2);msgg.delete();message.channel.send('*الكاست انرسل خلاص.*');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
-)})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('*الكاست تكنسل مدري ليه*')}
+message.channel.bulkDelete(2);msgg.delete();message.channel.send('الكاست انرسل خلاص.');message.guild.members.map(member => {setTimeout(() => {member.send(bc)}
+)})}if(XD.first().content == 'n') {message.channel.bulkDelete(2);message.channel.send('الكاست تكنسل مدري ليه')}
 })}).catch(myst =>{msgg.edit('Timed out.');})})
 }if(collected.first().content === '5'){} // لو تبي تضيف شي خامس :]
 }).catch(mys =>{msg.edit('Timed out to chose.')})})}});
